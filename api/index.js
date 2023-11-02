b7b02f8d7b5cac89b96505325052dbf055db6abd
HanaAnadolli <86631574+HanaAnadolli@users.noreply.github.com>
2023-11-03 00:14:25 +0100

    database added

--- api/index.js
+++ api/index.js
@@ -1,7 +1,50 @@
-import express from "express";
-
+const express = require('express');
 const app = express();
+const cors = require('cors');
 
-app.listen(8000, () => {
-    console.log("server is running on port 8000");
-});
+app.use(cors({
+    origin: '*'
+}));
+
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://127.0.0.1:27017/real-estate";
+const dbName = "real-estate";
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
+async function createDatabaseIfNotExists() {
+  try {
+    const adminDb = mongoose.connection.db.admin();
+    const dbNames = await adminDb.listDatabases();
+    if (dbNames.databases.some(db => db.name === dbName)) {
+      console.log('Database already exists:', dbName);
+    } else {
+      await adminDb.command({ create: dbName });
+      console.log('Database created:', dbName);
+    }
+  } catch (error) {
+    console.error('Error creating database:', error);
+  }
+}
+
+async function startServer() {
+  await connectToDatabase();
+  await createDatabaseIfNotExists();
+
+  app.listen(8000, function () {
+    console.log('Server is running');
+  });
+}
+
+startServer();
