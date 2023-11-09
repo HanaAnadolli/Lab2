416a4c29e7c69addcd06c992deb85d6b1de9a762
HanaAnadolli <hanadolli02@gmail.com>
2023-11-09 22:43:43 +0100

    sign out functionality added

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -60,10 +60,20 @@ const google = async (req, res, next) => {
     } catch (error) {
       next(error)
     }
+}
+
+const signOut = async (req, res, next) => {
+  try{
+    res.clearCookie('access_token');
+    res.status(200).json('User has been logged out!');
+  } catch (error) {
+    next(error);
   }
+}
 
 module.exports = {
     signup,
     signin,
     google,
+    signOut,
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { signup, signin, google } = require('../controller/auth.controller.js');
+const { signup, signin, google, signOut } = require('../controller/auth.controller.js');
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.post('/google', google);
+router.get('/signout', signOut);
 
 module.exports = router;
