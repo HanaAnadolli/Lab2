086dbf90b7fc8480e99d5cbadb7644f8b1a72f5a
HanaAnadolli <hanadolli02@gmail.com>
2023-11-15 01:18:53 +0100

    update list added

--- api/routes/listing.router.js
+++ api/routes/listing.router.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const {createListing, deleteListing} = require('../controller/listing.controller');
+const {createListing, deleteListing, updateListing, getListing} = require('../controller/listing.controller');
 const verifyToken = require('../utils/verifyUser');
 
 const router = express.Router();
 
 router.post('/create',verifyToken , createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
+router.get('/get/:id', getListing);
 
 module.exports = router;
