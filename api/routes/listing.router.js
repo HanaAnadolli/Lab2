a941780e9d4240cc0390e8a9bcb2e7a64c3cd12c
HanaAnadolli <hanadolli02@gmail.com>
2023-11-17 01:04:21 +0100

    search function on back added

--- api/routes/listing.router.js
+++ api/routes/listing.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {createListing, deleteListing, updateListing, getListing} = require('../controller/listing.controller');
+const {createListing, deleteListing, updateListing, getListing, getListings} = require('../controller/listing.controller');
 const verifyToken = require('../utils/verifyUser');
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/create',verifyToken , createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
+router.get('/get', getListings);
 
 module.exports = router;
